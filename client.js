53c7d3829aa13ddbb029b506dce187d4510e1ac4
Evgeny Chernikov <chernikovevge@gmail.com>
2020-11-12 15:56:44 +0300

    added history saving

--- client.js
+++ client.js
@@ -1,49 +1,52 @@
-const button = document.querySelector(".sendmessage")
-const msg = document.querySelector(".inputmessage")
-const messages = document.querySelector(".outputmessages")
-const status = document.querySelector(".status")
+const button = document.querySelector('.sendmessage')
+const msg = document.querySelector('.inputmessage')
+const messages = document.querySelector('.outputmessages')
+const status = document.querySelector('.status')
 const userName = `user${~~(Math.random() * 10)}`
 
-const ws = new WebSocket("ws://localhost:8080")
+const ws = new WebSocket('ws://localhost:8080')
 
-ws.onopen = (event)=>{
-    status.innerHTML = "Client Status: Online"
-
+ws.onopen = (event) => {
+  status.innerHTML = 'Client Status: Online'
 }
 
-ws.onclose = (event)=>{
-    status.innerHTML = "Client Status: Offline"
+ws.onclose = (event) => {
+  status.innerHTML = 'Client Status: Offline'
 }
 
-ws.onmessage = (event)=>{
-    const newMessage = `<p> ${event.data} </p>`
-    const messageNode = document.createElement('div')
-    messageNode.innerHTML = newMessage
-    messages.append(messageNode)
+ws.onmessage = (event) => {
+  const ms = JSON.parse(event.data)
+  const newMessage = `<p> ${ms.user} : ${ms.text} </p>`
+  const messageNode = document.createElement('div')
 
+  messageNode.innerHTML = newMessage
+  messages.append(messageNode)
 }
 
-ws.onerror = (event)=>{
-    status.innerHTML = `Client status: Offline, Error Code: ${event.code}`
-
+ws.onerror = (event) => {
+  status.innerHTML = `Client status: Offline, Error Code: ${event.code}`
 }
 
+function buttonHandler (event) {
+  event.preventDefault()
 
-function buttonHandler(event){
-    event.preventDefault()
-    const message = `${userName} : ${msg.value}`
-    if (msg.value === ""){
-        alert('Invalid input')
-    }else{
-        ws.send(message)
-        msg.value = ""
-    }
+  const message = {
+    user: userName,
+    text: msg.value
+  }
 
+  if (msg.value === '') {
+    alert('Invalid input')
+  } else {
+    ws.send(JSON.stringify({ ...message }))
+    msg.value = ''
+  }
 }
-button.addEventListener("click",buttonHandler)
 
-document.addEventListener('keydown',  (event)  => {
-    if (event.key === 'Enter') {
-        buttonHandler(event)
-    }
+button.addEventListener('click', buttonHandler)
+
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    buttonHandler(event)
+  }
 })
--- server.js
+++ server.js
@@ -1,38 +1,42 @@
 const maxMessagesOnServer = 100
 
-const WebSocket = require("ws")
+const WebSocket = require('ws')
 
-const wss = new WebSocket.Server({port:8080})
-
-let messages = []
-let clients = []
-
-wss.on("connection", ws =>{
-    if (!clients.includes(ws)){
-        clients.push(ws)
-        console.log('New client has connected')
-        for (let i = 0; i <= messages.length; i++){
-            if (messages.length > 1){
-                ws.send(messages[i])
-            }
-
-        }
-    }
-    ws.on("message", data =>{
-        if (messages.length >= maxMessagesOnServer){
-            messages = messages.slice(50)
-        }
-        messages.push(data)
-
-        clients.forEach(client => {
-            if (client.readyState === WebSocket.OPEN) {
-                client.send(data)
-            }
-        })
-             console.log(`Somethig went wrong with the message: ${e.data}  `)
-    })
+const wss = new WebSocket.Server({ port: 8080 }, () => {
+  console.log(`Server is running at 8080 port`)
 })
 
+let messages = []
+const clients = new Set()
 
+wss.on('connection', ws => {
+  if (!clients.has(ws)) {
+    console.log('New client has connected')
 
+    for (let msg of messages) {
+      if (messages.length) {
+        ws.send(JSON.stringify(msg))
+      }
+    }
 
+    clients.add(ws)
+  }
+
+  ws.on('message', data => {
+    if (messages.length >= maxMessagesOnServer) {
+      messages = messages.slice(50)
+    }
+
+    const msg = JSON.parse(data)
+
+    messages.push(msg)
+    clients.forEach(client => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify(msg))
+      }
+    })
+  })
+  ws.on('close', () => {
+    clients.delete(ws)
+  })
+})
